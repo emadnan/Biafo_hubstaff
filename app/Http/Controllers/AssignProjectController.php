508a9c2640c7fee206b9571462eb3384e607cac9
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-03-21 12:52:40 +0500

    fixes into add assign projects

--- app/Http/Controllers/AssignProjectController.php
+++ app/Http/Controllers/AssignProjectController.php
@@ -8,16 +8,12 @@ class AssignProjectController extends Controller
 {
     public function assign_projects(Request $request){
         $project_id = $request->project_id;
-        $start_date = $request->start_date;
-        $dead_line = $request->dead_line;
         $user_ids = $request->user_ids;
         foreach($user_ids as $user_id)
         {
             $assign = new AssignProject;
             $assign->project_id = $project_id;
             $assign->user_id = $user_id;
-            $assign->start_date = $start_date;
-            $assign->dead_line = $dead_line;
             $assign->save();
         }
         return response()->json(['message'=>'Assign Projects Successfully']);
@@ -42,8 +38,6 @@ class AssignProjectController extends Controller
     public function update_assign_projects(Request $request){
         
         $project_id = $request->project_id;
-        $start_date = $request->start_date;
-        $dead_line = $request->dead_line;
         $user_ids = $request->user_ids;
         $Project_id = \Request::input('project_id');
         $assign = AssignProject::where('Project_id',$Project_id)->delete();
@@ -52,8 +46,6 @@ class AssignProjectController extends Controller
             $assign = new AssignProject;
             $assign->project_id = $project_id;
             $assign->user_id = $user_id;
-            $assign->start_date = $start_date;
-            $assign->dead_line = $dead_line;
             $assign->save();
         }
 
