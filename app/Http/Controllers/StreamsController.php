ce29c60f58ff3d83b7bcc9ac0827eba4df6ff0d6
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-03-29 12:51:48 +0500

    fixes get streams

--- app/Http/Controllers/StreamsController.php
+++ app/Http/Controllers/StreamsController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\streams;
+use App\Models\Streams;
 use Illuminate\Http\Request;
 
 class StreamsController extends Controller
 {
     public function get_streams()
     {
-        $streams = streams::get();
+        $streams = Streams::get();
         return response()->json(['Streams' => $streams]);
     }
 }
