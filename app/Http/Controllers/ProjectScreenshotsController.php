c0ee2bf000dfec513ce860e28085ef71e81d0d86
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-04-05 12:38:10 +0500

    fixes into get project screenshots API

--- app/Http/Controllers/ProjectScreenshotsController.php
+++ app/Http/Controllers/ProjectScreenshotsController.php
@@ -103,10 +103,11 @@ class ProjectScreenshotsController extends Controller
     }
 
     public function getProjectScreenshots(){
-        $projectscreenshot= ProjectScreenshots::select('project_screenshots.*','project_screenshots.*','project_screenshots_timings.*','projects.*','projects.id as projects_id','project_screenshots_attachments.*','project_screenshots_attachments.id as project_screenshots_attachments_id')
+        $projectscreenshot= ProjectScreenshots::select('project_screenshots.*','project_screenshots.*','project_screenshots_timings.*','projects.*','projects.id as projects_id','project_screenshots_attachments.*','project_screenshots_attachments.id as project_screenshots_attachments_id','users.*')
         ->join('project_screenshots_timings','project_screenshots_timings.project_screenshorts_id','=','project_screenshots.id')
         ->join('project_screenshots_attachments','project_screenshots_attachments.project_screenshorts_timing_id','=','project_screenshots_timings.id')
         ->join('projects','projects.id','=','project_screenshots.project_id')
+        ->join('users','users.id','=','project_screenshots.user_id')
         ->where('date',date('Y-m-d'))
         ->get();
 
