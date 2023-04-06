ad25c3ed9398007da6c45e5dec0561db8fbdf0a3
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-04-06 12:56:58 +0500

    fixes into role has permissions

--- app/Http/Controllers/PermissionsRoleController.php
+++ app/Http/Controllers/PermissionsRoleController.php
@@ -10,19 +10,24 @@ class PermissionsRoleController extends Controller
 {
     public function add_Role_Permissions(Request $request){
         $rolePermisson= PermissionsRole::where('role_id',$request->role_id);
-        if($rolePermisson)
-        {
+        if($rolePermisson)   {
+
             $rolePermisson->delete();
+
         }
+
         $role_id = $request->role_id;
         $permissions = $request->permissions;
         foreach($permissions as $permission)
         {
+
             $permissionsRole = new PermissionsRole;
             $permissionsRole->role_id = $role_id;
             $permissionsRole->permission_id = $permission;
             $permissionsRole->save();
+
         }
+
         return response()->json(['message'=>'Add Permissions As Role Successfully']);
     }
 }
