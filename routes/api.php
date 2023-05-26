563e27e50187fbb428350cafb6e6025c1c7c16c7
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-05-26 11:57:58 +0500

    fixes into API for get screenShots by compny ID

--- routes/api.php
+++ routes/api.php
@@ -245,7 +245,7 @@ Route::get('/get_company_by_company_id/{id}',[CompanyController::class,'get_comp
 Route::get('/get_projectscreenshot_by_date/{date1}/{date2}/{user_id}',[ProjectScreenshotsController::class,'getProjectScreenshotsByDate']);
 
 //get screenshots by date and company_id
-Route::get('/get_projectscreenshot_by_date/{date1}/{date2}/{company_id}',[ProjectScreenshotsController::class,'getProjectScreenshotsByDateWithCompanyId']);
+Route::get('/get_projectscreenshot_by_compny_id/{date1}/{date2}/{company_id}',[ProjectScreenshotsController::class,'getProjectScreenshotsByDateWithCompanyId']);
 
 // Add FSF API\
 Route::post('/addFunctionalSpecificationForm/{step_no}',[FunctionalSpecificationFormController::class,'addFunctionalSpecificationForm']);
