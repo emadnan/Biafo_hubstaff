fe0bc16855cad38c20bc8a81973c210af7844223
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-05-31 14:30:50 +0500

    fix join for get functional_lead name into get fsf by id API

--- app/Http/Controllers/FunctionalSpecificationFormController.php
+++ app/Http/Controllers/FunctionalSpecificationFormController.php
@@ -103,7 +103,7 @@ class FunctionalSpecificationFormController extends Controller
         $id = \Request::input('id');
         $Functional = FunctionalSpecificationForm::
         select('users.*','users.name')
-        ->join('users','user.id','=','functional_specification_form.functional_lead_id')
+        ->join('users','users.id','=','functional_specification_form.functional_lead_id')
         ->where('id',$id)
         ->with('getFsfParameter')
         ->get();
