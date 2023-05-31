2a5ba5fbdf92ad71f11775e2d9d01176e27b9743
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-05-31 15:11:34 +0500

    fixes into get fsf by id API

--- app/Models/FunctionalSpecificationForm.php
+++ app/Models/FunctionalSpecificationForm.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FunctionalSpecificationForm extends Model
 {
@@ -11,6 +12,9 @@ class FunctionalSpecificationForm extends Model
     protected $table="functional_specification_form";
     protected $primaryKey="id";
 
+    function function_lead_name(){
+        return $this->BelongsTo('App\Models\User','id','functional_lead_id');
+    }
     function getFsfParameter(){
         return $this->hasMany('App\Models\FsfHasParameter', 'fsf_id', 'id');
     }
