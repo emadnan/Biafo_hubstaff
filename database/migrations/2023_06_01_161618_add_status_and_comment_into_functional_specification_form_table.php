d0924cd9c144c5f6914c3caa2e35d28855f2d8a8
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-06-01 16:22:40 +0500

    add status and cumment into functionalspecificationTable table

--- database/migrations/2023_06_01_161618_add_status_and_comment_into_functional_specification_form_table.php
+++ database/migrations/2023_06_01_161618_add_status_and_comment_into_functional_specification_form_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddStatusAndCommentIntoFsfAssignToUsersTable extends Migration
+class AddStatusAndCommentIntoFunctionalSpecificationFormTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class AddStatusAndCommentIntoFsfAssignToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('fsf_assign_to_users', function (Blueprint $table) {
-            
+        Schema::table('functional_specification_form', function (Blueprint $table) {
             $table->enum('status', ['Pending', 'InProgress', 'Completed'])->default('Pending')->after('dead_line');
             $table->string('comment')->after('status');
-
         });
     }
 
@@ -28,10 +26,9 @@ class AddStatusAndCommentIntoFsfAssignToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('fsf_assign_to_users', function (Blueprint $table) {
+        Schema::table('functional_specification_form', function (Blueprint $table) {
             $table->dropColumn('status');
             $table->dropColumn('comment');
-
         });
     }
 }
