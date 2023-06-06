c24076e02aff91e2b140c46f470fa1fadceed33d
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-06-06 16:06:35 +0500

    add join into get tasks by id API with users and project

--- app/Http/Controllers/TaskManagementController.php
+++ app/Http/Controllers/TaskManagementController.php
@@ -38,7 +38,7 @@ class TaskManagementController extends Controller
 
     function getTaskById($id){
         $task = TaskManagement::
-        select('task_managements.*','users.*','projects.*','task_managements.id as task_managements_id','users.id as users_id')
+        select('users.*','projects.*')
         ->join('users','users.id','=','task_managements.user_id')
         ->join('projects','projects.id','=','task_managements.project_id')
         ->where('id',$id)
