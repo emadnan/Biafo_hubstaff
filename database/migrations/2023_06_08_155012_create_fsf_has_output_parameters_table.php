8de4e3db50c136884dbbe721c2694fa7ee240753
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-06-08 15:53:56 +0500

    create fsf_has_output_parameter table

---
+++ database/migrations/2023_06_08_155012_create_fsf_has_output_parameters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFsfHasOutputParametersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fsf_has_output_parameters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('fsf_id');
+            $table->string('description');
+            $table->string('output_parameter_name');
+            $table->string('field_technical_name');
+            $table->string('field_length');
+            $table->string('field_type');
+            $table->string('field_table_name');
+            $table->string('mandatory_or_optional');
+            $table->string('parameter_or_selection');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fsf_has_output_parameters');
+    }
+}
