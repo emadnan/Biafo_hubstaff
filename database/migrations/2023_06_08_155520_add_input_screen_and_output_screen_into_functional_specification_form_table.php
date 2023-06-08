f4ed03f7dd063b4b7ea41fd3516e87dcc07e9a85
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-06-08 15:59:58 +0500

    add input_screen and output_screen into functional_specification_form

---
+++ database/migrations/2023_06_08_155520_add_input_screen_and_output_screen_into_functional_specification_form_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInputScreenAndOutputScreenIntoFunctionalSpecificationFormTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('functional_specification_form', function (Blueprint $table) {
+            $table->string('input_screen')->after('development_logic')->nullable();
+            $table->string('output_screen')->after('input_screen')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('functional_specification_form', function (Blueprint $table) {
+            $table->dropColumn('input_screen');
+            $table->dropColumn('output_screen');
+        });
+    }
+}
