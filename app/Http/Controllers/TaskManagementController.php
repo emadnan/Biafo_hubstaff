23d72b89b91866e9c3ee6859eb378ac445efbf38
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-06-09 15:40:27 +0500

    fixes into get tasks API

--- app/Http/Controllers/TaskManagementController.php
+++ app/Http/Controllers/TaskManagementController.php
@@ -29,7 +29,7 @@ class TaskManagementController extends Controller
     function getTasks(){
         
         $task = TaskManagement::
-        select('task_managements.*','users.*','projects.*','task_managements.id as task_managements_id')
+        select('task_managements.*','users.*','projects.*','task_managements.id as task_managements_id','task_managements.start_date as task_managements_start_date','task_managements.dead_line as task_managements_dead_line')
         ->join('users','users.id','=','task_managements.user_id')
         ->join('projects','projects.id','=','task_managements.project_id')
         ->with('team_lead_details')
