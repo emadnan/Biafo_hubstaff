ce824cbfe42ea2c303efd99b019243f77038d4cb
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-06-16 10:32:59 +0500

    add status and comment into functional_specification_form table

---
+++ database/migrations/2023_06_16_102948_add_status_and_comments_into_functional_specification_form_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusAndCommentsIntoFunctionalSpecificationFormTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('functional_specification_form', function (Blueprint $table) {
+            $table->enum('status', ['Pending', 'InProgress', 'Completed'])->default('Pending')->after('output_screen');
+            $table->string('comment')->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('functional_specification_form', function (Blueprint $table) {
+            
+            $table->dropColumn('status');
+            $table->dropColumn('comment');
+        });
+    }
+}
