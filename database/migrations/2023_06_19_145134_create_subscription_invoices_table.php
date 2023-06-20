8df32696973cb46efd96e504015869fcc8349e49
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-06-20 15:17:43 +0500

    changing into subscription_invoices table fealds types

--- database/migrations/2023_06_19_145134_create_subscription_invoices_table.php
+++ database/migrations/2023_06_19_145134_create_subscription_invoices_table.php
@@ -16,9 +16,9 @@ class CreateSubscriptionInvoicesTable extends Migration
         Schema::create('subscription_invoices', function (Blueprint $table) {
             $table->id();
             $table->string('stripe_id');
-            $table->string('company_id');
-            $table->string('subscription_id');
-            $table->string('amount');
+            $table->integer('company_id');
+            $table->integer('subscription_id');
+            $table->double('amount');
             $table->string('start_date');
             $table->string('end_date');
             $table->Integer('is_active')->default(1);
