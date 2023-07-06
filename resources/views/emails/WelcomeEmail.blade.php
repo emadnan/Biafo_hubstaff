37724be9998113fb5f9ebf0af5d3b50e59c08b0b
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-06 12:38:33 +0500

    fixes into add user email

--- resources/views/emails/WelcomeEmail.blade.php
+++ resources/views/emails/WelcomeEmail.blade.php
@@ -29,8 +29,8 @@
         <p>This is a quick reminder that in order to start tracking time, you need to download the desktop app. WorkLog for Windows.</p>
         <p>Click the download button below:</p>
         
-        <a href= "https://mega.nz/file/1bY1QbgD#mTHJZW-_uNdD5GmNjeYAjkdWuZizvQvTL9gC7CLlRdc">Download Here</a>
-
+        <a href= "https://mega.nz/file/oPw1jTDJ#RHzYigSuEFTweGfgqMKMkq4pDBd-yX3oIepPcDBGnMw">Download Here</a>
+        
         <p>Your Username and password are below:</p>
         <p>Username: {{ $data['email'] }} </p>
         <p>Password: {{ $data['password'] }}</p>
