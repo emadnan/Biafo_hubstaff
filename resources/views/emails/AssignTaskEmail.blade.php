880bd4d8d09b96a5bffabcdfd54bae540bf338a3
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-06 16:19:32 +0500

    assign task email

--- resources/views/emails/AssignTaskEmail.blade.php
+++ resources/views/emails/AssignTaskEmail.blade.php
@@ -14,6 +14,7 @@
         }
 
         .card {
+            margin-top:30px;
             padding: 50px;
             border: 1px solid #ccc;
             border-radius: 5px;
@@ -27,7 +28,7 @@
 
   <body>
       <div class="card">
-        
+
           <h2>Task Assignment</h2>
           <p>Dear {{ $data['userName'] }}!,</p>
           <p>I hope this email finds you well. I am reaching out to assign a task to you that requires your expertise and
