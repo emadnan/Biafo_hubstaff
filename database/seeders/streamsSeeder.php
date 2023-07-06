04bcd9b3831216917d2eb0669470178442ca4703
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-06 11:21:16 +0500

    add streams into stream seeder

--- database/seeders/streamsSeeder.php
+++ database/seeders/streamsSeeder.php
@@ -15,48 +15,62 @@ class streamsSeeder extends Seeder
     public function run()
     {
         $stream =  [
-            [
-                'id' => 1,
-                'project_id' => '5',
-                'user_id' => '10',
-                'company_id'=>'1',
-                'stream_name' => 'Web_Front_End'
+            // [
+            //     'id' => 1,
+            //     'project_id' => '5',
+            //     'user_id' => '10',
+            //     'company_id'=>'1',
+            //     'stream_name' => 'Web_Front_End'
 
-              ],
-            [
-                'id' => 2,
-                'project_id' => '5',
-                'user_id' => '11',
-                'company_id'=>'1',
-                'stream_name' => 'Back_End'
-              ],
-             [
-                'id' => 3,
-                'project_id' => '5',
-                'user_id' => '12',
-                'company_id'=>'1',
-                'stream_name' => 'Desktop_Front_End'
-             ],
+            //   ],
+            // [
+            //     'id' => 2,
+            //     'project_id' => '5',
+            //     'user_id' => '11',
+            //     'company_id'=>'1',
+            //     'stream_name' => 'Back_End'
+            //   ],
+            //  [
+            //     'id' => 3,
+            //     'project_id' => '5',
+            //     'user_id' => '12',
+            //     'company_id'=>'1',
+            //     'stream_name' => 'Desktop_Front_End'
+            //  ],
+            //   [
+            //     'id' => 4,
+            //     'project_id' => '5',
+            //     'user_id' => '12',
+            //     'company_id'=>'1',
+            //     'stream_name' => 'Idle Time'
+            //   ],
+            //   [
+            //     'id' => 5,
+            //     'project_id' => '5',
+            //     'user_id' => '12',
+            //     'company_id'=>'7',
+            //     'stream_name' => 'Service'
+            //   ],
+            //   [
+            //     'id' => 6,
+            //     'project_id' => '5',
+            //     'user_id' => '12',
+            //     'company_id'=>'7',
+            //     'stream_name' => 'Sale'
+            //   ],
               [
-                'id' => 4,
-                'project_id' => '5',
-                'user_id' => '12',
-                'company_id'=>'1',
-                'stream_name' => 'Idle Time'
-              ],
-              [
-                'id' => 5,
+                'id' => 7,
                 'project_id' => '5',
                 'user_id' => '12',
                 'company_id'=>'7',
-                'stream_name' => 'Service'
+                'stream_name' => 'MentorShip'
               ],
               [
-                'id' => 6,
+                'id' => 8,
                 'project_id' => '5',
                 'user_id' => '12',
                 'company_id'=>'7',
-                'stream_name' => 'Sale'
+                'stream_name' => 'Learning'
               ]
            ];
      
