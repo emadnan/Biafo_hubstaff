fbfec05cce5c970801d7fbcd9968450216485a2e
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-06 14:36:46 +0500

    email debuging into Task assigns

--- app/Http/Controllers/TaskManagementController.php
+++ app/Http/Controllers/TaskManagementController.php
@@ -27,10 +27,10 @@ class TaskManagementController extends Controller
         $task->save();
         
         $task1 = TaskManagement::
-            select('task_managements.*', 'users.email as user_email', 'projects.project_name')
+            select('task_managements.*', 'task_managements.id as task_management_id','users.email as user_email', 'projects.project_name')
             ->join('users', 'users.id', '=', 'task_managements.user_id')
             ->join('projects', 'projects.id', '=', 'task_managements.project_id')
-            ->where('task_managements.id', $task->id)
+            ->where('task_management_id', $task->id)
             ->with('team_lead_details')
             ->first();
         print_r($task1);
