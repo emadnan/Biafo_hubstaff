9bddc8947bb7002988369515bb1340285816ebeb
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-14 17:19:10 +0500

    add column into change_request_forms

---
+++ database/migrations/2023_07_14_171705_add_colomn_into_change_request_forms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColomnIntoChangeRequestFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('change_request_forms', function (Blueprint $table) {
+            $table->string('crf_version');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('change_request_forms', function (Blueprint $table) {
+            $table->dropColumn('crf_version');
+        });
+    }
+}
