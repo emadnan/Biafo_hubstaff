17dd2de44e89983d667550829977d82804082f37
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-18 12:09:39 +0500

    fixes into get CRF by id API

--- app/Models/ChangeRequestForm.php
+++ app/Models/ChangeRequestForm.php
@@ -4,10 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ChangeRequestForm extends Model
 {
     use HasFactory;
     protected $table="change_request_forms";
     protected $primaryKey="id";
+
+    function project_details(){
+        return $this->BelongsTo('App\Models\Project','project_id','id');
+    }
+
+    function module_details(){
+        return $this->BelongsTo('App\Models\Module','module_id','id');
+    }
+
+    function company_details(){
+        return $this->hasMany('App\Models\Company', 'company_id', 'id');
+    }
+
+    function FSF_details(){
+        return $this->hasMany('App\Models\FunctionalSpecificationForm', 'fsf_id', 'id');
+    }
 }
