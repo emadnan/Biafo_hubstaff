480fe2d6acc16e8485d49e100d46e909229bdff9
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-18 14:48:04 +0500

    fixes get fsf s by module_id and project_id

--- routes/api.php
+++ routes/api.php
@@ -329,7 +329,7 @@ Route::post('/deleteFunctionalSpecificationForm',[FunctionalSpecificationFormCon
 Route::get('/getFunctionalSpecificationForm',[FunctionalSpecificationFormController::class,'getFunctionalSpecificationForm']);
 
 // get All FSF by module_id and project_id API\
-Route::get('/getFunctionalSpecificationFormByProjectIdAndModuleId',[FunctionalSpecificationFormController::class,'getFunctionalSpecificationFormByProjectIdAndModuleId']);
+Route::get('/getFsfByProjectIdAndModuleId',[FunctionalSpecificationFormController::class,'getFsfByProjectIdAndModuleId']);
 
 // get FSF By Id API\
 Route::get('/getFunctionalSpecificationFormById/{fsf}',[FunctionalSpecificationFormController::class,'getFunctionalSpecificationFormById']);
