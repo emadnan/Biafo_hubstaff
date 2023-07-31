ea8613e944280abdfb04ebcf708fcff07aba0227
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-07-31 11:51:15 +0500

    chat crud APIs

---
+++ app/Http/Controllers/ChatBoxController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatBox;
+use Illuminate\Http\Request;
+
+class ChatBoxController extends Controller
+{
+    function sendMessage(){
+        $chat = new ChatBox();
+        $chat->crf_id = \Request::input('crf_id');
+        $chat->sender_id = \Request::input('sender_id');
+        $chat->messages = \Request::input('messages');
+        $chat->message_time = \Request::input('message_time');
+        $chat->save();
+        return response()->json(['message'=>'Send Message successfully']);
+    }
+
+    public function getAllMessage()
+    {
+        $chat = ChatBox::
+        join('users','users.id','=','chat_box.sender_id')
+        ->get();
+        return response()->json(['chat' => $chat]);
+    }
+}
