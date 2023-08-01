ad0b341369df4ef30d45772bd14f48120c47cdcc
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-08-01 11:28:10 +0500

    add time and date  auto

--- app/Http/Controllers/ChatBoxController.php
+++ app/Http/Controllers/ChatBoxController.php
@@ -4,20 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\ChatBox;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ChatBoxController extends Controller
 {
     function sendMessage()  {
-
+        
+        // Assuming you have imported the necessary classes here.
+    
         $chat = new ChatBox();
         $chat->crf_id = \Request::input('crf_id');
         $chat->sender_id = \Request::input('sender_id');
         $chat->messages = \Request::input('messages');
-        $chat->message_time = \Request::input('message_time');
-        $chat->save();
         
-        return response()->json(['message'=>'Send Message successfully']);
+        // Get the current date and time in the desired format (using Carbon).
+        $message_time = Carbon::now()->format('Y-m-d H:i:s');
+        $chat->message_time = $message_time;
+        
+        $chat->save();
+    
+        return response()->json(['message' => 'Send Message successfully']);
     }
+    
+    
+    
+    
 
     public function getAllMessage()    {
 
