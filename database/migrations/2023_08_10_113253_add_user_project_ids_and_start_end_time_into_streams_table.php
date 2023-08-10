db54abbe5b61fbccc1ff49c80d552297c1f1d78b
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-08-10 14:39:33 +0500

    remove user_id from streams table

--- database/migrations/2023_08_10_113253_add_user_project_ids_and_start_end_time_into_streams_table.php
+++ database/migrations/2023_08_10_113253_add_user_project_ids_and_start_end_time_into_streams_table.php
@@ -14,8 +14,7 @@ class AddUserProjectIdsAndStartEndTimeIntoStreamsTable extends Migration
     public function up()
     {
         Schema::table('streams', function (Blueprint $table) {
-            $table->integer('user_id')->after('company_id')->nullable();
-            $table->integer('project_id')->after('user_id')->nullable();
+            $table->integer('project_id')->after('company_id')->nullable();
             $table->dateTime('start_time')->after('stream_name')->nullable();
             $table->dateTime('end_time')->after('start_time')->nullable();
 
@@ -30,7 +29,6 @@ class AddUserProjectIdsAndStartEndTimeIntoStreamsTable extends Migration
     public function down()
     {
         Schema::table('streams', function (Blueprint $table) {
-            $table->dropColumn('user_id');
             $table->dropColumn('project_id');
             $table->dropColumn('start_time');
             $table->dropColumn('end_time');
