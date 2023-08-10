a8b909d95056764bb801a1a5d8dc72cd8f10769f
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-08-10 15:21:19 +0500

    make join with projects and streams table

--- app/Models/Streams.php
+++ app/Models/Streams.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Streams extends Model
 {
     use HasFactory;
     protected $table="streams";
     protected $primaryKey="id";
+
+    function projectDetails(){
+        return $this->BelongsTo('App\Models\projects','project_id','id');
+    }
 }
