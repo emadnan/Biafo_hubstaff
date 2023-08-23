238f4caac5eede03a2728795ba9bec35cb589893
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-08-23 14:44:45 +0500

    fixes get assigned type id through table id

--- app/Http/Controllers/StreamsController.php
+++ app/Http/Controllers/StreamsController.php
@@ -169,9 +169,8 @@ class StreamsController extends Controller
         return response()->json(['Streams' => $stream]);
     }
 
-    function getAssignedTypeId(Request $request) {
-        $id = $request->input('id');
-    
+    function getAssignedTypeId($id) {
+        
         $assignedType = StreamsHasUser::where('id', $id)
             ->pluck('assigning_type_id')
             ->first(); 
