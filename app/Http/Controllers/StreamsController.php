53228efbae5432b5b18e026361fbe62eecb99a54
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-08-29 11:37:39 +0500

    debuging

--- app/Http/Controllers/StreamsController.php
+++ app/Http/Controllers/StreamsController.php
@@ -140,9 +140,8 @@ class StreamsController extends Controller
         
         $totalAssigningTypeId = StreamsHasUser::where('user_id', $userId)
             ->sum('assigning_type_id');
-            print_r(($totalAssigningTypeId + $assigning_type_id));
-            exit();
-        if (($totalAssigningTypeId + $assigning_type_id) < 3) {
+
+        if (($totalAssigningTypeId + $assigning_type_id) <= 3) {
             
             if ($assigning_type_id < 3) {
                 $allowedCount = 3; // Allow adding assigning_type_id when assigning_type is 1 or 2
