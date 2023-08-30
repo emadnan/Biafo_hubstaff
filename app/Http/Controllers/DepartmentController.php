7a7df104f22b7195732d191f793355b453f571a1
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-08-30 15:36:03 +0500

    fixes into delete department with projects with FSF ,Crf, Streams

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -8,6 +8,8 @@ use App\Models\FunctionalSpecificationForm;
 use App\Models\ChangeRequestForm;
 use App\Models\FsfHasParameter;
 use App\Models\FsfHasOutputParameter;
+use App\Models\Streams;
+use App\Models\StreamsHasUser;
 use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
@@ -46,26 +48,50 @@ class DepartmentController extends Controller
         return response()->json(['Departments' => $department]);
     }
 
-    function delete_department(){
+    function delete_department()    {
+
         $id = \Request::input('id');
         
         $projects = Project::where('department_id', $id)->get();
-    
-        foreach ($projects as $project)    {
+        
+        foreach ($projects as $project) {
+            
+            $fsfDeletes = FunctionalSpecificationForm::where('project_id', $project->id)->get();
+            
+            foreach ($fsfDeletes as $fsfDelete) {
+                
+                FsfHasParameter::where('fsf_id', $fsfDelete->id)->delete();
+                FsfHasOutputParameter::where('fsf_id', $fsfDelete->id)->delete();
+            }
             
             FunctionalSpecificationForm::where('project_id', $project->id)->delete();
-            FsfHasParameter::where('fsf_id', $project->id)->delete();
-            FsfHasOutputParameter::where('fsf_id', $project->id)->delete();
+            
+            $crfDeletes = ChangeRequestForm::where('project_id', $project->id)->get();
+            
+            foreach ($crfDeletes as $crfDelete) {
+                
+                ChangeRequestSummary::where('crf_id', $crfDelete->id)->delete();
+            }
+            
             ChangeRequestForm::where('project_id', $project->id)->delete();
-            ChangeRequestSummary::where('project_id', $project->id)->delete();
-
+            
+            $streamDeletes = Streams::where('project_id', $project->id)->get();
+            
+            foreach ($streamDeletes as $streamDelete) {
+                
+                StreamsHasUser::where('stream_id', $streamDelete->id)->delete();
+            }
+            
+            Streams::where('project_id', $project->id)->delete();
+            
+            $project->delete();
         }
-    
-        Project::where('department_id', $id)->delete();
+        
         Department::where('id', $id)->delete();
-    
+        
         return response()->json(['message' => 'Delete Department and associated projects and forms successfully']);
     }
+    
     
 
     public function get_department_by_id($id)   {
