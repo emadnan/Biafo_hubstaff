0ef03937de89b0c25b8ca6ffea6613e340a0545e
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2023-10-30 10:39:59 +0500

    Delete older than 2 months recode.

---
+++ app/Console/Commands/DeleteOldData.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ProjectScreenshotsAttechments;
+use App\Models\ProjectScreenshotsTiming;
+use Illuminate\Console\Command;
+
+class DeleteOldData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete:previous_data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'For delete All old data till two months ago';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // $twoMonthsAgo = now()->subMonths(2);
+        $firstDayOfPresentMonth = now()->startOfMonth();
+        $twoMonthsAgo = $firstDayOfPresentMonth->subMonth(2);
+        $project_ss = ProjectScreenshotsAttechments::where('created_at', '<', $twoMonthsAgo)
+            ->get();
+        foreach ($project_ss as $project) {
+            if ($project->path_url != '') {
+                $url = $project->path_url;
+                $filename = pathinfo($url, PATHINFO_BASENAME);
+                $filePath = public_path('screenshots/' . $filename);
+
+                if (file_exists($filePath)) {
+                    unlink($filePath);
+                }
+            }
+            $project->delete();
+        }
+        ProjectScreenshotsTiming::where('created_at', '<', $twoMonthsAgo)
+            ->delete();
+        $this->info('Old data deleted successfully.');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,13 +9,18 @@ class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
+     * 
+     * 
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
+    protected $commands = [
+        Commands\DeleteOldData::class,
+    ];
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('delete:old-data')->dailyAt('00:00');
     }
 
     /**
