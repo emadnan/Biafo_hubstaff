a67e9e146b659f75b41928b6c92e64b65e714b35
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2024-04-30 10:39:23 +0500

    fixes into team has users by team lead id

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -1,30 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Team;
-use App\Models\User;
 
+use App\Models\Team;
 use App\Models\TeamHasUser;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
-    function createTeam(Request $request){
+    public function createTeam(Request $request)
+    {
 
         $team = new Team();
         $team->company_id = \Request::input('company_id');
         $team->department_id = \Request::input('department_id');
         $team->team_lead_id = \Request::input('team_lead_id');
         $team->team_name = \Request::input('team_name');
-        $team->description  = \Request::input('description');
+        $team->description = \Request::input('description');
         $team->save();
 
-        return response()->json(['message'=>'Add Team successfully']);
+        return response()->json(['message' => 'Add Team successfully']);
     }
 
-    function updateTeam($id){
+    public function updateTeam($id)
+    {
 
-        $role = Team::where('id',$id)
+        $role = Team::where('id', $id)
             ->update([
                 'team_lead_id' => \Request::input('team_lead_id'),
                 'department_id' => \Request::input('department_id'),
@@ -43,65 +45,70 @@ class TeamController extends Controller
         return response()->json(['Teams' => $team]);
     }
 
-    function deleteTeam($id)   {
+    public function deleteTeam($id)
+    {
 
-        $team = Team::where('id',$id)->delete();
+        $team = Team::where('id', $id)->delete();
 
-        return response()->json(['message'=>'delete Team successfully']);
+        return response()->json(['message' => 'delete Team successfully']);
     }
 
     public function getTeamById($team_id)
     {
         $team = Team::
-        where('id',$team_id)->get();
+            where('id', $team_id)->get();
 
         return response()->json(['Team' => $team]);
     }
-    function teamHasUsers(Request $request) {
+    public function teamHasUsers(Request $request)
+    {
 
         $userIds = $request->input('user_ids');
         $teamId = $request->input('team_id');
-    
+
         TeamHasUser::where('team_id', $teamId)->delete();
-    
+
         foreach ($userIds as $userId) {
             $teamUser = new TeamHasUser;
             $teamUser->team_id = $teamId;
             $teamUser->user_id = $userId;
             $teamUser->save();
         }
-    
+
         return response()->json(['message' => 'Users added to the team successfully']);
     }
 
-    Function getUsersByTeamLeadId($team_lead_id){
+    public function getUsersByTeamLeadId($team_lead_id)
+    {
 
-        $team = Team::where('team_lead_id',$team_lead_id)->first();
-        
-        $teamUsers = TeamHasUser::where('team_id', $team->id)
-        ->join('users', 'team_has_users.user_id', '=', 'users.id')
-        ->select('users.name')
-        ->get();
+        $team = Team::where('team_lead_id', $team_lead_id)->first();
+
+        $teamUsers = TeamHasUser::where('team_has_users.team_id', $team->id)
+            ->join('users', 'team_has_users.user_id', '=', 'users.id')
+            ->select('users.name')
+            ->get();
 
         return response()->json(['team' => $teamUsers]);
     }
 
-    function getTeamLeadByCompanyId($company_id){
+    public function getTeamLeadByCompanyId($company_id)
+    {
         $users = User::
-        where('company_id',$company_id)
-        ->where('role',7)
-        ->get();
+            where('company_id', $company_id)
+            ->where('role', 7)
+            ->get();
 
         return response()->json(['Team_Leads' => $users]);
     }
 
-    function getTeamsByDepartmentId($department_id){
+    public function getTeamsByDepartmentId($department_id)
+    {
         $team = Team::join('departments', 'teams.department_id', '=', 'departments.id')
-        ->where('departments.id', $department_id)
-        ->select('teams.*','departments.*')
-        ->get();
+            ->where('departments.id', $department_id)
+            ->select('teams.*', 'departments.*')
+            ->get();
 
         return response()->json(['team' => $team]);
     }
-    
+
 }
