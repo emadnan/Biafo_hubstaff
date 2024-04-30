83c9c3d3667859974229f1b8aa3727f0f8c5c6e9
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2024-04-30 12:37:47 +0500

    fixes into daily repoting for company

--- app/Http/Controllers/ProjectScreenshotsController.php
+++ app/Http/Controllers/ProjectScreenshotsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\ProjectScreenshots;
 use App\Models\ProjectScreenshotsAttechments;
 use App\Models\ProjectScreenshotsTiming;
