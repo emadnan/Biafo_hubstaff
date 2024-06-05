b86429d9d4eaca8755def4c98fd5cc5d7ebfb3cb
MiAwan <103881058+MiAwan3332@users.noreply.github.com>
2024-06-05 13:15:27 +0500

    create API of getting data between 2 dates and company_id

--- app/Http/Controllers/ProjectScreenshotsController.php
+++ app/Http/Controllers/ProjectScreenshotsController.php
@@ -882,13 +882,13 @@ class ProjectScreenshotsController extends Controller
             $totalMinutes = floor(($totalTime % 3600) / 60);
             $totalSeconds = $totalTime % 60;
 
-            $data[$date][$screenshot->user_id]['id'] = $screenshot->user_id;
-            $data[$date][$screenshot->user_id]['name'] = $screenshot->name;
-            $data[$date][$screenshot->user_id]['company_id'] = $screenshot->company_id;
-            $data[$date][$screenshot->user_id]['totalHours'] = $totalHours;
-            $data[$date][$screenshot->user_id]['totalMinutes'] = $totalMinutes;
-            $data[$date][$screenshot->user_id]['totalSeconds'] = $totalSeconds;
-            $data[$date][$screenshot->user_id]['status'] = 'online';
+            $data['id'] = $screenshot->user_id;
+            $data['name'] = $screenshot->name;
+            $data['company_id'] = $screenshot->company_id;
+            $data['totalHours'] = $totalHours;
+            $data['totalMinutes'] = $totalMinutes;
+            $data['totalSeconds'] = $totalSeconds;
+            $data['status'] = 'online';
         }
 
         return response()->json(['data' => $data]);
